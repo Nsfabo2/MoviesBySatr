a0856dcb42ddcdc4e4cd46ab44f54a0db7fc3722
Najla Abothnain <na.abothnain@gmail.com>
2022-02-18 22:56:06 +0300

    Initial Commit

---
+++ MoviesBySatr/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MoviesBySatr
+//
+//  Created by Najla on 16/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //attrbuites
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func SearchForMovie(){
+        
+    }
+    func AddMovie(){
+        
+    }
+    func UpdateMovie(){
+        
+    }
+    func DeleteMovie(){
+        
+    }
+}
+
